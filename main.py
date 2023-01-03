a1397b59d465a4f157c5327c6eae58c86ba86e4f
Bryantw12 <b.ashton522@gmail.com>
2023-01-03 10:51:02 -0500

    almost complete 1st

--- main.py
+++ main.py
@@ -22,20 +22,42 @@ c = Counter(ip_address)
 newDict = dict()
 
 for key, value in c.items():
-    if value >=200:
+    if value >=400:
         newDict[key] = value
 
 
 print(newDict.keys())
 print(newDict.values())
 
-plt.bar(newDict.keys(), newDict.values())
+barWidth = 0.9
+
+plt.bar(newDict.keys(), newDict.values(), width = barWidth, color = (0.3,0.1,0.4,0.6), label='Alone')
+
+ 
+# Create bars
+barWidth = 0.9
+bars1 = [3, 3, 1]
+bars2 = [4, 2, 3]
+bars3 = [4, 6, 7, 10, 4, 4]
+bars4 = bars1 + bars2 + bars3
+ 
+# The X position of bars
+r1 = [1,5,9]
+r2 = [2,6,10]
+r3 = [3,4,7,8,11,12]
+r4 = r1 + r2 + r3
+ 
+# Create legend
+plt.legend()
+
+plt.xticks(rotation=30, ha='right')
+
+plt.show()
 
 b = ip_address
 
 plt.xticks(rotation=30)
 
-plt.xticks(rotation=30, ha='right')
 
 print(newDict)
 
